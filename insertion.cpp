36c4d4f690c26e39e11d5bfe73f7154b619dba7c
vinayak majhi <vinayak.djp@gmail.com>
2025-06-20 17:05:34 +0530

    insertion optimized

--- insertion.cpp
+++ insertion.cpp
@@ -7,13 +7,20 @@ void insertion_sort(int arr[], int n){
 
     for(int i = 0; i<=n-1; i++){
             int j = i;
+            bool didswap = false;
             while(j>0 && arr[j-1] > arr[j]){
                 int temp = arr[j-1];
                 arr[j-1] = arr[j];
                 arr[j] = temp;
                 j--;
+                didswap = true;
+            }
+
+            if(!didswap && i == n-1){
+                break;
             }
     }
+
 
     cout<<"AFTER insertion: "<< endl;
     for(int i=0; i<n;i++){
@@ -26,7 +33,7 @@ void insertion_sort(int arr[], int n){
 }
 
 int main(){
-    int arr[] = {204,25,5,77,9,11} ;
+    int arr[] = {1,5,7,83,99,100 };//{204,25,5,77,9,11} ;
     int n = sizeof(arr)/ sizeof(arr[0]);
     cout<<"before insertion sort: " <<endl;
     for(int i = 0; i<n; i++){
