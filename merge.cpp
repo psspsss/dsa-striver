8fc96bce3698084d4e18bffffa179ca92908d368
vinayak majhi <vinayak.djp@gmail.com>
2025-06-22 14:28:15 +0530

    merge sort

--- merge.cpp
+++ merge.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void merge(vector <int> &arr, int low, int mid, int high){
 
-void merge(vector <int> &arr, int low,int mid, int high){
     vector <int> temp;
     int left = low;
     int right = mid+1;
 
-    while(left <= mid && right <=high){
-        if(arr[left] <= arr[right]){
+    while(left <=mid && right <=high){
+        if(arr[left]<=arr[right]){
             temp.push_back(arr[left]);
             left++;
         }
 
-        else {
+        else{
             temp.push_back(arr[right]);
             right++;
         }
@@ -24,42 +24,44 @@ void merge(vector <int> &arr, int low,int mid, int high){
         left++;
     }
 
-    while(right<=high){
+    while (right<=high){
         temp.push_back(arr[right]);
         right++;
     }
 
-     for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
+    for(int i = low; i<=high; i++){
+        arr[i]= temp[i-low];
+        }
 }
 
 
-void mergeSort(vector <int> &arr, int low , int high){
-    if(low>=high){return;}
+void mergeSort(vector <int> &arr, int low, int high){
+    if(low>=high)return;
     int mid = (low+high)/2;
     mergeSort(arr, low, mid);
     mergeSort(arr, mid+1, high);
     merge(arr, low ,mid, high);
 }
 
+
 int main(){
-    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+     
+    vector <int> arr = {4,6 ,66,3,22,1, 25};
     int n = 7;
 
-    cout << "Before MergeSort Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
+    cout<<"Before MergeSort: " <<endl;
+    for(int i=0;i<n;i++){
+        cout<< arr[i] <<" ";
     }
-    cout << endl;
+    cout<<endl;
 
     mergeSort(arr, 0, n-1);
 
-    cout << "After MergeSort Array: " << endl;
+    cout << "After MergeSort: " << endl;
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " "  ;
     }
     cout << endl;
-    return 0 ;
+
     return 0;
 }
