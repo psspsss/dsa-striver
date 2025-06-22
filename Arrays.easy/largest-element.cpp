ece6c8ef62c1fe7d10faec53be6404c19fe173c6
vinayak majhi <vinayak.djp@gmail.com>
2025-06-22 18:02:21 +0530

    q1 arrays easy

---
+++ Arrays.easy/largest-element.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int largest = nums[0];
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
+        return largest;
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 3, 0, 99, -40};
+    Solution sol;
+    int result = sol.largestElement(nums);
+    cout << "The largest element is: " << result << endl;
+
+    return 0;
+}
